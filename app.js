193f6ed3bf7336a7b809fb938ee5dea6e9593c75
Ngozi <hexwhyzed01@gmail.com>
2026-02-22 06:13:37 +0100

    Set up app.js, with packgages imported, server connected, and base endpoint

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const helmet = require("helmet");
+require("dotenv").config();
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", (req, res, next) => {
+  res.send("Service home");
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log("Server Listening");
+});
